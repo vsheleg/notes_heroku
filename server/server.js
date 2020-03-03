9e8ed08d776092ae68cac2a27362757499a32ceb
Varvara Sheleh <varvara.sheleh@itechart-group.com>
2020-03-03 15:57:49 +0300

    ff

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const express = require("express"),
   notesRouter = require("./routes/note.router"),
   userRouter = require("./routes/user.router");
 const path = require("path");
-const port = process.env.PORT || 8080;
 
 const app = express();
 app.use(cors());
@@ -13,6 +12,7 @@ app.use(express.static(path.join(__dirname, "build")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/*", function(req, res) {
+  console.log(__dirname);
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 app.get("/", function(req, res) {
