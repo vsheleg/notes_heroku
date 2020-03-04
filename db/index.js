85db3c6f514f0c18346b97d34c9676e2cc30810e
Varvara Sheleh <varvara.sheleh@itechart-group.com>
2020-03-04 12:47:10 +0300

    add db url

--- db/index.js
+++ db/index.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
-  port: process.env.PORT,
+  port: match[4],
+  port: match[3],
   logging: true
 });
 
