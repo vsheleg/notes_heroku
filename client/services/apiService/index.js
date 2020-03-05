be21c9eb14ab8e8775e3905431da834550476643
Varvara Sheleh <varvara.sheleh@itechart-group.com>
2020-03-05 11:32:10 +0300

    add url

--- client/services/apiService/index.js
+++ client/services/apiService/index.js
@@ -1,4 +1,4 @@
-const BASEURI = "https://calm-river-32384.herokuapp.com/";
+const BASEURI = "https://notes-frontend-app.herokuapp.com/";
 const KEY = "note-token";
 
 async function request(path, params = {}) {
--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, "postgres", "", {
   dialect: "postgres",
   protocol: "postgres",
   logging: true
