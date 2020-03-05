aa50285197f1ecdaf026703ff93655f133b9c977
Varvara Sheleh <varvara.sheleh@itechart-group.com>
2020-03-05 12:15:43 +0300

    last changes

---
+++ src/client/services/apiService/index.js
@@ -0,0 +1,39 @@
+const BASEURI = process.env.PORT;
+const KEY = "note-token";
+
+async function request(path, params = {}) {
+  alert(BASEURI + path);
+  const token = await localStorage.getItem(KEY);
+  return fetch(BASEURI + path, {
+    ...params,
+    headers: {
+      Authorization: token,
+      "Content-Type": "application/json",
+      ...(params.headers || {})
+    }
+  }).then(response => {
+    if (response.status !== 403) {
+      return response.json();
+    } else {
+      return { error: response };
+    }
+  });
+}
+function deleteData(url) {
+  return request(url, {
+    method: "delete"
+  });
+}
+
+function get(url) {
+  return request(url);
+}
+
+function post(url, data) {
+  return request(url, {
+    method: "POST",
+    body: JSON.stringify(data)
+  });
+}
+
+export default { get, post, deleteData };
