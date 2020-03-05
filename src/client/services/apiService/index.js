0d42840fcf5ccb957c1e8cea0a8d0bb70affac34
Varvara Sheleh <varvara.sheleh@itechart-group.com>
2020-03-05 12:29:05 +0300

    new changes

--- src/client/services/apiService/index.js
+++ src/client/services/apiService/index.js
@@ -2,7 +2,6 @@ const BASEURI = "https://calm-river-32384.herokuapp.com/";
 const KEY = "note-token";
 
 async function request(path, params = {}) {
-  alert(BASEURI + path);
   const token = await localStorage.getItem(KEY);
   return fetch(BASEURI + path, {
     ...params,
