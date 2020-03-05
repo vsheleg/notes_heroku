e0b53136d1bff19dca68f829e95d842fdfb0d225
Varvara Sheleh <varvara.sheleh@itechart-group.com>
2020-03-05 11:56:07 +0300

    add url

---
+++ build/precache-manifest.15a9622fc5b8d882ff91e81130329817.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee55248ec75d773bae4ecd5c8e9260c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79485670c1c49514b2a6",
+    "url": "/static/css/main.f840f128.chunk.css"
+  },
+  {
+    "revision": "e8b779650653485c3395",
+    "url": "/static/js/2.465bc971.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.465bc971.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79485670c1c49514b2a6",
+    "url": "/static/js/main.43792945.chunk.js"
+  },
+  {
+    "revision": "8eb86d135bc80d76d217",
+    "url": "/static/js/runtime-main.afb0aeba.js"
+  },
+  {
+    "revision": "bf2628e43f68f292fa97d1a51a9b0a73",
+    "url": "/static/media/close.bf2628e4.png"
+  },
+  {
+    "revision": "8eaff2655eaf4dc3dbe888abc24a8de6",
+    "url": "/static/media/logout.8eaff265.png"
+  }
+]);
