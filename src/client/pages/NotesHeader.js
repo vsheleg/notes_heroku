c223e76e1dded2233acb492e341e37075807ec01
Varvara Sheleh <varvara.sheleh@itechart-group.com>
2020-03-09 16:37:09 +0300

    new changes

--- src/client/pages/NotesHeader.js
+++
@@ -1,7 +0,0 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import React from "react";
-import "./main.css";
-
-export default function NotesHeader({}) {
-  return <div id="notes-header">Notes</div>;
-}
